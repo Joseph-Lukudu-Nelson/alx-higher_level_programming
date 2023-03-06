fb8287d71eafa260f19948e8cf70654a7560d13e
Joseph-Lukudu-Nelson <jkudus2022@gmail.com.com>
2023-03-06 05:34:29 -0800

    4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,5 @@
 -- a scripts that create table inside database
-FROM WHERE DATABASE hbtn_0c_0; CREATE TABLE IF NOT EXISTS first_table;
+USE bhn_0c_0; CREATE TABLE IF NOT EXISTS first_table(
+id INT, 
+name VARCHAR(256)
+);
