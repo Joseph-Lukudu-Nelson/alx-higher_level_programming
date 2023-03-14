208b57ccb1456d86dd21f7c3dded27e5c2abb91e
Joseph-Lukudu-Nelson <jkudus2022@gmail.com.com>
2023-03-14 10:31:51 -0700

    A script that print 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-/// a scripts that print multiple line
+// A scripts that print multiple line
 
-const my1 = 'C is fun \n' +
+const myvar = 'C is fun \n' +
 'Python is cool\n' +
 'JavaScript is amazing';
-console.log(my1);
+console.log(myvar);
